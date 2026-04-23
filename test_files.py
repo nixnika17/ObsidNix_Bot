f1d2cf2da1271c84df91e139e4b1931d6d620e59
NIX VAIL <vasilishenanika@gmail.com>
2026-04-23 19:10:53 +0300

    Initial commit for Obsidian Bot

---
+++ test_files.py
@@ -0,0 +1,6 @@
+# this is a test for ObsidianNix Bot.I'm testing to open and write in local vaules.
+# PROTOTYPE №1
+
+# for program can open files alone i need to show where and how she can do it?but how?
+def files():
+    pass 
