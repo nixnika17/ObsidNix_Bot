4088d8411b495876c1844b964da29d4090ebbe97
NIX VAIL <vasilishenanika@gmail.com>
2026-04-26 21:45:29 +0300

    Оновлено код

--- test_files.py
+++ test_files.py
@@ -7,42 +7,54 @@ import os  #operation system
 # for program can open files alone i need to show where and how she can do it?but how?
 
 class ObsidianManager:
-    def __init__(self, vault_path):
+    def __init__(self, parent_path):
         #  here incapsulation for not change 
-         self._vault_path = vault_path # Ми просто зберігаємо переданий шлях в self
+         self._parent_path = parent_path # Ми просто зберігаємо переданий шлях в self
 
     def create_folder(self, folder_name): #склеювання шляху,perent vault
-            path = os.path.join(self._vault_path, folder_name)
+            path = os.path.join(self._parent_path, folder_name)
             # Наприклад, те що ввів користувач:
     # exist_ok=True -  не дає програмі впасти, якщо папка вже є
             os.makedirs(path, exist_ok=True)
             return path
 
-    # def list_folders(self):
-    #     # Цей метод покаже всі папки всередині твого Обсідіана
-    #     try:
-    #         items = os.listdir(self.vault_path)
-    #         # Фільтруємо тільки папки, ігноруємо системні файли
-    #         folders = [f for f in items if os.path.isdir(os.path.join(self.vault_path, f))]
-    #         return folders
-    #     except FileNotFoundError:
-    #         return "Помилка: Шлях не знайдено. Перевір адресу папки!"
+    def list_folders(self):
+        # Цей метод покаже всі папки всередині твого Обсідіана
+        try:
+            items = os.listdir(self.parent_path)
+            # Фільтруємо тільки папки, ігноруємо системні файли
+            folders = [f for f in items if os.path.isdir(os.path.join(self.parent_path, f))]
+            return folders
+        except FileNotFoundError:
+            return "Помилка: Шлях не знайдено. Перевір адресу папки!"
+#  ----------Make file
+    def make_note(self,file_name,content):
+         full_patch = os.path.join(self._parent_path, f"{file_name}.md") 
+         with open(full_patch, mode = 'a',encoding='utf-8') as file:
+              file.write(f"{content}/n")
 
+         return full_patch
+         
+        
+    
+         
+
+    
     # def write_note(self, folder_name, file_name, content):
     #     # Створюємо повний шлях до файлу
     #     folder_path = os.path.join(self.vault_path, folder_name)
     #     full_path = os.path.join(folder_path, f"{file_name}.md")
 
-    #     # Відкриваємо файл для допису (режим 'a' - append)
-    #     with open(full_path, 'a', encoding='utf-8') as file:
-    #         file.write(f"\n{content}")
-    #     print(f"Нотатку збережено у: {full_path}")
+        # # Відкриваємо файл для допису (режим 'a' - append)
+        # with open(full_path, 'a', encoding='utf-8') as file:
+        #     file.write(f"\n{content}")
+        # print(f"Нотатку збережено у: {full_path}")
 
 
 # Заміни цей шлях на свій справжній шлях до Obsidian
-my_vault = r"C:\Users\vasil\OneDrive\MYWORLD\! PROJECTS\obsidNix_Bot\TEST(DELETE.LATER)"
+parent_path = r"C:\Users\vasil\OneDrive\MYWORLD\! PROJECTS\obsidNix_Bot\TEST(DELETE.LATER)"
 
-manager = ObsidianManager(my_vault)
+manager = ObsidianManager(parent_path)
 
 # Отримуємо назву від користувача
 user_choice = input("Яку папку створити в Obsidian? ")
