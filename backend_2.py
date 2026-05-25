1b284a83ec64f16c7aacfca5b72ac6042280bea4
NIX VAIL <vasilishenanika@gmail.com>
2026-05-25 21:57:57 +0300

    new

--- backend_2.py
+++ backend_2.py
@@ -2,7 +2,23 @@
 
 import os # operation system, manage folders and other
 
-# open and edit files
-def make_file(file, mode='a', encoding = 'utf-8', newline=True):
+print("i work!")
 
-    pass
+class Obsidian_Note: 
+    def __init__(self, path): # definition path
+      if not path:
+            raise ValueError("❌ path is not exist!")
+      self._path = path  # here incapsulation for not change 
+    
+      
+    def make_file(file): # open and edit files
+      with open(mode='a', encoding = 'utf-8', newline=True) as f:
+       print('\n', file=f)
+
+
+path = r"G:\My Drive\NIX WORKSHOP"
+
+
+# git add .                                   # Додає всі зміни
+# git commit -m "new" 
+# git push    
