ed92a5e7ea0ea1820bbb1f32204288592d286851
NIX VAIL <vasilishenanika@gmail.com>
2026-05-27 23:43:45 +0300

    new not work

--- backend_2.py
+++ backend_2.py
@@ -10,15 +10,21 @@ class Obsidian_Note:
             raise ValueError("❌ path is not exist!")
       self._path = path  # here incapsulation for not change 
     
-      
-    def make_file(file): # open and edit files
-      with open(mode='a', encoding = 'utf-8', newline=True) as f:
-       print('\n', file=f)
+    os.makedirs("dir", exist_ok = True)
+    def make_file(self, file_name): # open and edit files
+        file_name = print("file name? :    ")
+        full_path = os.path.join(self._path, file_name)
+
+        if not file_name:
+           file_name = "file_obs"
+        with open(file_name, mode='a', encoding = 'utf-8', newline='') as f:
+         print('', file=f)
+        print("sucsess")
 
 
 path = r"G:\My Drive\NIX WORKSHOP"
 
-
+# python backend_2.py
 # git add .                                   # Додає всі зміни
 # git commit -m "new" 
 # git push    
